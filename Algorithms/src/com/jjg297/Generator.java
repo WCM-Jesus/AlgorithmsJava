d6454d184f025f587c695b8fab83deb552af934f
Jesus Garcia <garciajj730@gmail.com>
2017-10-22 21:41:55 -0700

    Created the Random Array Generator

--- Algorithms/src/com/jjg297/Generator.java
+++ Algorithms/src/com/jjg297/Generator.java
@@ -1,10 +1,19 @@
 package com.jjg297;
-import java.math.
+
+import java.util.Random;
 
 public class Generator {
 
     public int[] generate_array(int size)
     {
+        int[] return_array = new int[size];
+        Random random_gen = new Random();
 
+        for(int i=0; i<size; i++)
+        {
+            return_array[i] = random_gen.nextInt();
+        }
+
+        return return_array;
     }
 }
